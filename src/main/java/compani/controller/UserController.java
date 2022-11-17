11411cdfbd35af1b9e8a8054eb615f3fe61bc540
vahzatsaava <waja11@mail.ru>
2022-11-17 19:47:44 +0400

    comments

--- src/main/java/compani/controller/UserController.java
+++ src/main/java/compani/controller/UserController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-@WebServlet("/")
+@WebServlet("/getUser")
 public class UserController extends HttpServlet {
     private UserService service;
 
@@ -23,16 +23,11 @@ public class UserController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String action = req.getServletPath();
-        switch (action){
-            case "/allUsers":
-                getAll(req,resp);
-                break;
-            case "/getUserId":
-                getById(req,resp);
-                break;
-            default:
-                break;
+        String id = req.getParameter("id");
+        if (id == null) {
+            getAll(req, resp);
+        } else {
+            getById(Integer.parseInt(id), req, resp);
         }
     }
 
@@ -41,13 +36,11 @@ public class UserController extends HttpServlet {
         PrintWriter writer = resp.getWriter();
         String firstName = req.getParameter("first_name");
         String lastName = req.getParameter("last_name");
-        String action = req.getServletPath();
-        if (action.equals("/addUser")) {
-            if (firstName != null && lastName != null && !firstName.isEmpty() && !lastName.isEmpty()) {
-                service.add(new User(firstName, lastName));
-            } else {
-                writer.println("Please, enter your name and surname !");
-            }
+        resp.setContentType("application/json");
+        if (firstName != null && lastName != null && !firstName.isEmpty() && !lastName.isEmpty()) {
+            service.add(new User(firstName, lastName));
+        } else {
+            writer.println("Please, enter your name and surname !");
         }
     }
 
@@ -56,13 +49,9 @@ public class UserController extends HttpServlet {
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         int id = Integer.parseInt(req.getParameter("id_del"));
-        String action = req.getServletPath();
-        if (action.equals("/removeUser")){
-            service.delete(id);
-            resp.setContentType("text/html");
-            writer.println("the object was deleted");
-        }
-
+        resp.setContentType("application/json");
+        service.delete(id);
+        writer.println("the object was deleted");
     }
 
     @Override
@@ -70,27 +59,25 @@ public class UserController extends HttpServlet {
         int id = Integer.parseInt(req.getParameter("id"));
         String firstName = req.getParameter("first_name");
         String lastName = req.getParameter("last_name");
-        String action = req.getServletPath();
-        if (action.equals("/updateUser")){
-            service.update(new User(id,firstName,lastName));
+        resp.setContentType("application/json");
+        service.update(new User(id, firstName, lastName));
+    }
+
+    private void getAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
+        for (User user : service.getUsers()) {
+            writer.println("id<br>" + user.getId() + " : " + user.getFirstName() + ":" + user.getLastName());
         }
     }
 
-    private void getAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+    private void getById(Integer id, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        resp.setContentType("text/html");
-        for (User user:service.getUsers()) {
-            writer.println(    "id<br>" + user.getId() + " : " + user.getFirstName() + ":" + user.getLastName() );
-        }
-    }
-    private void  getById(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
-        PrintWriter writer = resp.getWriter();
-        int id = Integer.parseInt(req.getParameter("id"));
-        resp.setContentType("text/html");
+        resp.setContentType("application/json");
         User user = service.find(id);
-        if (user != null){
-            writer.println(  "id" + user.getId() + " : " + user.getFirstName() + ":" + user.getLastName() );
-        }else {
+        if (user != null) {
+            writer.println("id" + user.getId() + " : " + user.getFirstName() + ":" + user.getLastName());
+        } else {
             writer.println("This object not found by this id");
         }
     }
